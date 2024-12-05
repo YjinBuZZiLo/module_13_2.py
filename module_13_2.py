7ac14907f8033576faade24ca0eb660e22fbd196
YjinBuZZiLo <evgene2305@gmail.com>
2024-12-05 22:39:42 +0300

    Add files via upload

--- module_13_2.py
+++ module_13_2.py
@@ -1,24 +1,21 @@
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.utils import executor
+import asyncio
 
-API_TOKEN = '***'
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
-# Вместо *** стоит ввести токен бота
-
-bot = Bot(token=API_TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
 
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
+async def start_message(message):
     print('Привет! Я бот помогающий твоему здоровью.')
-    await message.reply('Привет! Я бот помогающий твоему здоровью.')
+
 
 @dp.message_handler()
-async def all_messages(message: types.Message):
+async def all_messages(message):
     print('Введите команду /start, чтобы начать общение.')
-    await message.reply('Введите команду /start, чтобы начать общение.')
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
